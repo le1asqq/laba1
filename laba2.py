b3e32c1db05387f2f0183488c4c7f550e9bbfd7e
le1asqq <161071510+le1asqq@users.noreply.github.com>
2024-03-09 01:21:35 +0400

    Create laba2.py

---
+++ laba2.py
@@ -0,0 +1,30 @@
+#Целые четные числа. Максимальное число выводить прописью.
+import re
+
+digit_words = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
+
+def num2words(num):
+    if num == 0:
+        return 'ноль'
+    result = ''
+    while num > 0:
+        digit = num % 10
+        result = digit_words[digit] + ' ' + result
+        num = num // 10
+    result = result.strip()
+    return result
+
+max_num = 0
+
+with open('input.txt', 'r') as f:
+    data = f.read()
+    nums = re.findall(r'\b\d+\b', data)
+    for num in nums:
+        num = int(num)
+        if num % 2 == 0:
+            lexeme = num2words(num)
+            if num > max_num:
+                max_num = num
+
+if max_num > 0:
+    print('Максимальное число: ' + num2words(max_num))
