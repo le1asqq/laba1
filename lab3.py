a0e8393debe15c1a73e020b8bb28bfeb178e836f
le1asqq <161071510+le1asqq@users.noreply.github.com>
2024-03-23 11:52:53 +0400

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,75 +1,103 @@
 import random
 
-# Ввод данных с клавиатуры
-K = int(input("Введите K: "))
-N = int(input("Введите N: "))
+# Вводим два числа K и N
+K = int(input("Введите число K: "))
+N = int(input("Введите число N: "))
 
-# Инициализация матрицы A(N,N)
-A = [[0 for _ in range(N)] for _ in range(N)]
+# Формируем матрицу А
+A = [[random.randint(-10, 10) for j in range(N)] for i in range(N)]
 
-# Заполнение подматриц B, C, D, E
-for i in range(N):
-    for j in range(N):
-        if i < N/2:
-            if j < N/2:
-                A[i][j] = random.randint(-10, 10)  # заполнение подматрицы B
-            else:
-                A[i][j] = random.randint(-10, 10)  # заполнение подматрицы C
-        else:
-            if j < N/2:
-                A[i][j] = random.randint(-10, 10)  # заполнение подматрицы D
-            else:
-                A[i][j] = random.randint(-10, 10)  # заполнение подматрицы E
+# Формируем матрицы B, C, D, E
+B = [[2 for j in range(N//2)] for i in range(N//2)]
+C = [[0, 0, 0, 0] for i in range(N//2)] + [[1, 3, 0, 0] for i in range(N//2, N)]
+D = [[4, 0, 0, 0] for i in range(N//2)] + [[0, 0, 0, 0] for i in range(N//2, N)]
+E = [[2 for j in range(N//2, N)] for i in range(N//2, N)]
 
-# Функция для вычисления количества нулевых элементов в области 4
-def count_zeros_in_area_4(matrix):
-    count = 0
-    for i in range(N//2 + N % 2, N):
+# Выводим матрицы B, C, D, E
+print("Матрица B:")
+for row in B:
+    print(row)
+
+print("Матрица C:")
+for row in C:
+    print(row)
+
+print("Матрица D:")
+for row in D:
+    print(row)
+
+print("Матрица E:")
+for row in E:
+    print(row)
+
+# Формируем матрицу F
+count_zeros = 0
+count_positives = 0
+
+for i in range(N//2):
+    if all(elem == 0 for elem in C[i]):
+        count_zeros += 1
+
+for i in range(N//2, N, 2):
+    for j in range(N//2):
+        if C[i][j] > 0:
+            count_positives += C[i][j]
+
+if count_zeros > count_positives:
+    for i in range(N//2):
         for j in range(N//2):
-            if matrix[i][j] == 0:
-                count += 1
-    return count
-
-# Функция для вычисления количества нулевых элементов в области 1
-def count_zeros_in_area_1(matrix):
-    count = 0
-    for i in range(N//2):
-        for j in range(i):
-            if matrix[i][j] == 0:
-                count += 1
-    for i in range(N//2, N):
-        for j in range(N-(i+1)):
-            if matrix[i][j] == 0:
-                count += 1
-    return count
-
-# Формирование матрицы F в зависимости от условия
-if count_zeros_in_area_4(A) > count_zeros_in_area_1(A):
-    # Поменять в В симметрично области 2 и 3 местами
-    for i in range(N//2):
-        for j in range(N//2, N):
-            A[i][j], A[N-i-1][j] = A[N-i-1][j], A[i][j]
+            B[i][j], B[j][i] = B[j][i], B[i][j]
 else:
-    # Поменять С и E местами несимметрично
-    for i in range(N//2 + N % 2):
-        for j in range(N//2):
-            A[i][j], A[N-i-1][N-j-1] = A[N-i-1][N-j-1], A[i][j]
+    C, E = E, C
 
-# Вывод матрицы A
-print("Матрица A:")
-for row in A:
-    print(' '.join([str(elem) for elem in row]))
+# Выводим матрицу F
+print("Матрица F:")
+for row in C:
+    print(row)
 
-# Вычисление выражения ((F*A)– (K * AT))
-result = [[0 for _ in range(N)] for _ in range(N)]
-AT = [[A[j][i] for j in range(N)] for i in range(N)]
+# Вычисляем выражение ((К*F)*А– (K * A^T))
+result = [[0 for j in range(N)] for i in range(N)]
+
 for i in range(N):
     for j in range(N):
         for k in range(N):
-            result[i][j] += A[i][k] * A[k][j]
-        result[i][j] -= K * AT[i][j]
+            result[i][j] += K * C[i][k] * A[k][j]
 
-# Вывод результата
-print("Результат выражения ((F*A)– (K * AT)):")
+for i in range(N):
+    for j in range(N):
+        result[i][j] -= K * A[j][i]
+
+# Выводим результат
+print("Результат:")
 for row in result:
-    print(' '.join([str(elem) for elem in row]))
+    print(row)
+
+# Считаем суммы в разных областях матрицы А
+sum_below_main = 0
+sum_above_main = 0
+sum_area1 = 0
+
+for i in range(N):
+    for j in range(i):
+        if j < i and j < N//2:
+            sum_below_main += A[i][j]
+
+for i in range(N):
+    for j in range(i, N):
+        if j >= i and j >= N//2:
+            sum_above_main += A[i][j]
+
+for i in range(N//2):
+    for j in range(i):
+        if j < i:
+            sum_area1 += A[i][j]
+
+for i in range(N//2, N):
+    for j in range(N-(i+1)):
+        if j < N//2:
+            sum_area1 += A[i][j]
+
+# Выводим суммы в разных областях матрицы А
+print("Сумма элементов ниже главной диагонали:", sum_below_main)
+print("Сумма элементов выше главной диагонали:", sum_above_main)
+print("Сумма элементов в области 1:", sum_area1)
