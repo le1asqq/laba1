52d52cde0734cdb4f32e6d90f4471f2a7301ddd3
le1asqq <161071510+le1asqq@users.noreply.github.com>
2024-04-06 11:13:06 +0400

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,22 +1,26 @@
-#Формируется матрица F следующим образом: если в С количество нулевых элементов в нечетных столбцах в области 4 больше, чем количество нулевых  элементов в четных столбцах в области 1, 
-#то поменять в В симметрично области 2 и 3 местами, иначе С и Е поменять местами несимметрично. 
-#При этом матрица А не меняется. После чего вычисляется выражение: ((F*A)– (K * AT) . Выводятся по мере формирования А, F и все матричные операции последовательно.#
+# Формируется матрица F следующим образом: если в С количество нулевых элементов в нечетных столбцах в области 4 больше, чем количество нулевых  элементов в четных столбцах в области 1, 
+# то поменять в В симметрично области 2 и 3 местами, иначе С и Е поменять местами несимметрично. 
+# При этом матрица А не меняется. После чего вычисляется выражение: ((F*A)– (K * AT) . Выводятся по мере формирования А, F и все матричные операции последовательно.
 
 import random
-# Вводим два числа K и N
+
+ Вводим два числа K и N
 K = int(input("Введите число K: "))
 N = int(input("Введите число N: "))
 
-# Формируем матрицу А
+
+ Формируем матрицу А
 A = [[random.randint(-10, 10) for j in range(N)] for i in range(N)]
 
-# Формируем матрицы B, C, D, E
+
+Формируем матрицы B, C, D, E
 B = [[2 for j in range(N//2)] for i in range(N//2)]
 C = [[0, 0, 0, 0] for i in range(N//2)] + [[1, 3, 0, 0] for i in range(N//2, N)]
 D = [[4, 0, 0, 0] for i in range(N//2)] + [[0, 0, 0, 0] for i in range(N//2, N)]
 E = [[2 for j in range(N//2, N)] for i in range(N//2, N)]
 
-# Выводим матрицы B, C, D, E
+
+ Выводим матрицы B, C, D, E
 print("Матрица B:")
 for row in B:
     print(row)
@@ -33,54 +37,53 @@ print("Матрица E:")
 for row in E:
     print(row)
 
-# Формируем матрицу F
-count_zeros = 0
-count_positives = 0
 
-for i in range(N//2):
-    if all(elem == 0 for elem in C[i]):
-        count_zeros += 1
+if count_zeros > count_positives: 
+    for i in range(N//2): 
+        for j in range(N//2): 
+            B[i][j], B[j][i] = B[j][i], B[i][j] 
 
-for i in range(N//2, N, 2):
-    for j in range(N//2):
-        if C[i][j] > 0:
-            count_positives += C[i][j]
 
-if count_zeros > count_positives:
+  count_zero_in_4 = 0
+    for i in range(len(С)//2,len(С)):
+        for j in range(len(С)-(i+1)+1,i,3):
+            if С [i][j] == 0:
+                count_zero_in_4 += 1
+
+
+    count_zeros_in_1 = 0
+    for i in range(len(С)//2):
+        for j in range(0,i,2):
+            if С [i][j] == 0:
+                count_zeros_in_1 += 1
+    for i in range(len(С)//2,len(С)):
+        for j in range(0,len(С)-(i+1),2):
+            if С [i][j] == 0:
+                count_zeros_in_1 += 1
+
+count_zeros_in_4 > count_zeros_in_1
     for i in range(N//2):
         for j in range(N//2):
             B[i][j], B[j][i] = B[j][i], B[i][j]
 else:
     C, E = E, C
 
-# Выводим матрицу F
+
+ Выводим матрицу F
 print("Матрица F:")
 for row in C:
     print(row)
 
-# Вычисляем выражение ((К*F)*А– (K * A^T))
+
+ Вычисляем выражение ((К*F)*А– (K * A^T))
 esult = [[0 for i in range(N)] for j in range(N)]
 
 for i in range(N):
     for j in range(N):
-        for k in range(N//2):  # Исправление: использование правильной размерности
+
+        for k in range(N//2):   Исправление: использование правильной размерности
             result[i][j] += K * C[i][k] * A[k][j]
 
-
-
-# Считаем суммы в разных областях матрицы А
-sum_below_main = 0
-sum_above_main = 0
-sum_area1 = 0
-# Сумма элементов ниже главной диагонали
-s_below_main = 0
-for i in range(n):
-    for j in range(i):
-        if j < i:
-            s_below_main += a[i][j]
-print("Сумма элементов ниже главной диагонали:", s_below_main)
-
-# Сумма элементов выше главной диагонали
 s_above_main = 0
 for i in range(n):
     for j in range(i, n):
@@ -88,36 +91,31 @@ for i in range(n):
             s_above_main += a[i][j]
 print("Сумма элементов выше главной диагонали:", s_above_main)
 
-# Сумма элементов в области 1
-s_area1 = 0
-for i in range(n//2):
-    for j in range(i):
-        if j < i:
-            s_area1 += a[i][j]
+Сумма в области 4
+ count = 0
+for i in range(len(C)//2,len(C)): 
+        for j in range(len(C)-(i+1)+1,i,3): 
+            if C [i][j] == 0: 
+                count += 1
 
-for i in range(n//2, n):
-    for j in range(n-(i+1)):
-        if j < n//2:
-            s_area1 += a[i][j]
-print("Сумма элементов в области 1:", s_area1)
 
-# Сумма элементов на главной диагонали
-s_main_diag = sum(a[i][i] for i in range(n))
-print("Сумма элементов на главной диагонали:", s_main_diag)
 
-# Сумма элементов на побочной диагонали
-s_side_diag = sum(a[i][n-(i+1)] for i in range(n))
-print("Сумма элементов на побочной диагонали:", s_side_diag)
+ Сумма элементов в области 1
 
-# Сумма элементов ниже главной диагонали
-s_below_main_diag = sum(a[i][j] for i in range(n) for j in range(i))
-print("Сумма элементов ниже главной диагонали:", s_below_main_diag)
+ count = 0
+for i in range(len(C)//2): 
+        for j in range(0,i,2): 
+            if C [i][j] == 0: 
+                count += 1 
+    for i in range(len(C)//2,len(C)): 
+        for j in range(0,len(C)-(i+1),2): 
+            if C [i][j] == 0: 
+                count += 1
 
-# Сумма элементов выше главной диагонали
-s_above_main_diag = sum(a[i][j] for i in range(n) for j in range(i+1,n))
-print("Сумма элементов выше главной диагонали:", s_above_main_diag)
 
-# Выводим результат
+
+
+Выводим результат
 print("Результат:")
 for row in result:
     print(row)
